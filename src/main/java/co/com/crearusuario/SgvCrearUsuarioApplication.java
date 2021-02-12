b1543045d882bd2ad50681da8a95839061a471de
Fabio <fabio12hq@hotmail.com>
2021-02-12 18:09:53 -0500

    commit1 12022020

---
+++ src/main/java/co/com/crearusuario/SgvCrearUsuarioApplication.java
@@ -0,0 +1,13 @@
+package co.com.crearusuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgvCrearUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgvCrearUsuarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/crearusuario/SgvCrearUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.crearusuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgvCrearUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
